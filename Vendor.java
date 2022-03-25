332b49909dc5cd0e913906f867e5216e37743d55
annabelgrice <annabelrice@wustl.edu>
2022-03-25 14:40:11 -0500

    magic number and bear bucks done

--- Vendor.java
+++ Vendor.java
@@ -1,6 +1,12 @@
 public class Vendor {
     public static void main(String[] args){
-
+        ArgsProcessor ap = new ArgsProcessor(args);
+        int bearBucks = ap.nextInt("how many bear bucks do you have in your account?");
+        public static void MagicRange(){
+            magicNumber= Math.random();
+            rangeHigh = magicNumber+2;
+            rangeLow= magicNumber-2;
+        }
     }
 
     
