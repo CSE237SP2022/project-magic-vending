1dda0294fc6a0a48dd44b44fa27d46e7dc30be46
annabelgrice <annabelrice@wustl.edu>
2022-03-25 14:46:43 -0500

    magic range

--- Vendor.java
+++ Vendor.java
@@ -1,12 +1,13 @@
 public class Vendor {
     public static void main(String[] args){
-        ArgsProcessor ap = new ArgsProcessor(args);
-        int bearBucks = ap.nextInt("how many bear bucks do you have in your account?");
-        public static void MagicRange(){
-            magicNumber= Math.random();
-            rangeHigh = magicNumber+2;
-            rangeLow= magicNumber-2;
-        }
+       
+         
+       
+    }
+    public static void MagicRange(){
+        int magicNumber= (int)Math.random();
+        int rangeHigh = magicNumber+2;
+        int rangeLow= magicNumber-2;
     }
 
     
