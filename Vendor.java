d6af57dba5219bc6ed1c927b50a3cc840d33a47f
annabelgrice <annabelrice@wustl.edu>
2022-03-25 16:02:26 -0500

    Update Vendor.java

--- Vendor.java
+++ Vendor.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Vendor {
  
     public static void main(String[] args){
-       
+        
          
        
     }
@@ -10,6 +10,19 @@ public class Vendor {
         int magicNumber= (int)Math.random();
         int rangeHigh = magicNumber+2;
         int rangeLow= magicNumber-2;
+    }
+    public static int BearBucksInput(){
+        Scanner input = new Scanner(System.in);
+        try{
+        System.out.print("Enter amount of Bear Bucks");
+        int bearBucks = input.nextInt();
+        return bearBucks;
+        }
+        finally{
+            input.close();
+        }
+        
+        
     }
 
     
