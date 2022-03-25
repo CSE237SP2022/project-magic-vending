352b4b7af00cf9b2383c0ac7a2f46cb1e41139bd
annabelgrice <54559843+annabelgrice@users.noreply.github.com>
2022-03-25 14:48:20 -0500

    Revert "Magic number bear bucks"

--- Vendor.java
+++ Vendor.java
@@ -1,13 +1,6 @@
 public class Vendor {
     public static void main(String[] args){
-       
-         
-       
-    }
-    public static void MagicRange(){
-        int magicNumber= (int)Math.random();
-        int rangeHigh = magicNumber+2;
-        int rangeLow= magicNumber-2;
+
     }
 
     
