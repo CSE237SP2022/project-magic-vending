e47b93c551de43f531d0aa3dec2f99397b68d424
caberghausen <caberghausen@users.noreply.github.com>
2022-04-06 15:25:38 -0500

    fixed print function

--- Vendor.java
+++ Vendor.java
@@ -19,8 +19,13 @@ public class Vendor {
         items[0][0] = new VendingItem(3, "🥑", "avocado", 2.50);
         items[0][1] = new VendingItem(3, "🌮", "taco", 3.70);
         items[0][2] = new VendingItem(3, "🍇", "grapes", 2.50);
-        items[0][3] = new VendingItem(2, "🍏", "apple", 1.20);
+        items[0][3] = new VendingItem(2, "🍏", "green apple", 1.20);
         items[0][4] = new VendingItem(4, "🥨", "pretzel", 3.10);
+        items[1][0] = new VendingItem(1, "🍎", "red apple", 1.10);
+        items[1][1] = new VendingItem(1, "🍑", "peach", 1.90);
+        items[1][2] = new VendingItem(1, "🍒", "cherries", 3.35);
+        items[1][3] = new VendingItem(3, "🍆", "eggplant", 2.65);
+        items[1][4] = new VendingItem(1, "🥖", "baguette", 2.95);
     }
 
     /**
@@ -49,7 +54,7 @@ public class Vendor {
     public void displayVendor(){
         System.out.println("\t1 \t2 \t3 \t4 \t5");
         System.out.println("-------------------------------------------");
-        for(int i = 0; i<1; i++){
+        for(int i = 0; i<2; i++){
             printLine(i);
         }
     }
@@ -64,6 +69,7 @@ public class Vendor {
             for(int y = 0; y < items[line].length; y++){
                 System.out.print("\t" + items[line][y].getPrice());
             }
+            System.out.println();
         }
 
     /**
